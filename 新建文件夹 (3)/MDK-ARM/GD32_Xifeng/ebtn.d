91e86a6fb27a00cb8a07830929c87f84108c8db4
WeZaoChuan <159777600+WeZaoChuan@users.noreply.github.com>
2026-04-28 00:38:21 +0800

    Delete 新建文件夹 (3) directory

--- 新建文件夹 (3)/MDK-ARM/GD32_Xifeng/ebtn.d
+++
@@ -1,5 +0,0 @@
-gd32_xifeng\ebtn.o: ..\Components\ebtn\ebtn.c
-gd32_xifeng\ebtn.o: D:\keil\ARM\ARM_compiler_5.06\Bin\..\include\string.h
-gd32_xifeng\ebtn.o: ..\Components\ebtn\ebtn.h
-gd32_xifeng\ebtn.o: D:\keil\ARM\ARM_compiler_5.06\Bin\..\include\stdint.h
-gd32_xifeng\ebtn.o: ..\Components\ebtn\bit_array.h
--- 新建文件夹 (3)/sysFunction/scheduler.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
-
-#include "mydefine.h"
-
-void scheduler_init(void);
-void scheduler_run(void);
-
-#endif
